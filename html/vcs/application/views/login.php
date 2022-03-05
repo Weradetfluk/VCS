531a14382ad6e9f293e8becd5bad8e9046758adc
Suwapat Saowarod <62160340@go.buu.ac.th>
2022-03-05 18:05:27 +0700

    62160340 Suwapat Saowarod build form login

--- html/vcs/application/views/login.php
+++ html/vcs/application/views/login.php
@@ -1,11 +1,37 @@
+<style>
+    .container-form {
+        display: flex;
+        justify-content: center;
+        flex-direction: row;
+        padding-top: 20px;
+    }
+</style>
+
 <form action="">
-    <div class="form-group">
-        <label for="username"><i class="fas fa-user"></i>Username</label>
-        <input class="form-control" type="text" name="username">
+    <div class="container container-form">
+        <div class="card">
+            <div class="card-header" style="text-align: center;">
+                Login
+            </div>
+            <div class="card-body">
+                <div class="row">
+                    <div class="form-group col">
+                        <label for="username"><span class="material-icons">account_circle</span></label>
+                        <input class="form-control" type="text" name="username" placeholder="Username">
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="form-group col">
+                        <label for="password"><span class="material-icons">password</span></label>
+                        <input class="form-control" type="text" name="password" placeholder="Password">
+                    </div>
+                </div>
+            </div>
+            <div class="card-footer" style="text-align: right;">
+                <button type="submit" class="btn btn-primary">Login</button>
+            </div>
+
+        </div>
     </div>
-    <div class="form-group">
-        <label for="password"><i class="fas fa-key"></i>password</label>
-        <input class="form-control" type="text" name="password">
-    </div>
-    <button type="submit" class="btn btn-primary">Login</button>
+
 </form>
--- html/vcs/application/views/v_header.php
+++ html/vcs/application/views/v_header.php
@@ -10,4 +10,4 @@
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 
 <!-- icon -->
-<link rel="stylesheet" href="<?php echo base_url().'assets/icon/'?>/icons.css" type="text/css">
+<link rel="stylesheet" href="<?php echo base_url().'asset/icon/material_icon.css'?>" type="text/css">
