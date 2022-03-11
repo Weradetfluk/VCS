589b3ed181a151cf095474a44c8dfbbb39e91bde
Naaka Punparich <62160082@go.buu.ac.th>
2022-03-12 01:36:51 +0700

    62160082 topbar

--- html/vcs/application/views/v_topbar.php
+++ html/vcs/application/views/v_topbar.php
@@ -1,5 +1,18 @@
+<style>
+  .make-nav {
+    text-decoration: none;
+    font-size: 18px;
+    padding-left: 16px;
+  }
+</style>
+
 <nav class="navbar navbar-dark bg-dark">
   <a class="navbar-brand" href="#">
     <i class="material-icons">how_to_vote</i> Vote Camp System
   </a>
+  <span class="navbar-text">
+    <a class="make-nav" href="#">จัดการผลโหวด</a>
+    <a class="make-nav" href="#">จัดการผู้ใช้งาน</a>
+    <a class="make-nav" href="#">ดูผลโหวด</a>
+  </span>
 </nav>
